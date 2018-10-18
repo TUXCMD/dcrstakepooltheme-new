e81ae6577a2fc77c5e03f6dbd589f0cc00285c9a
Mykolas <mykolas.raizys@gmail.com>
2018-10-18 16:51:01 +0300

    TrackDuck fix

--- js/global.js
+++ js/global.js
@@ -6,6 +6,35 @@ $('#sidebarCollapse').on('click', function () {
     $('#sidebar, .menu-trigger').addClass('active');
 });
 
+function submitState(el) {
+
+    var $form = $(el),
+        $requiredInputs = $form.find('input:required'),
+        $submit = $form.find('input[type="submit"]');
+
+    $submit.attr('disabled', 'disabled');
+
+    $requiredInputs.keyup(function () {
+
+      $form.data('empty', 'false');
+
+      $requiredInputs.each(function() {
+        if ($(this).val() === '') {
+          $form.data('empty', 'true');
+        }
+      });
+
+      if ($form.data('empty') === 'true') {
+        $submit.attr('disabled', 'disabled').attr('title', 'fill in all required fields');
+      } else {
+        $submit.removeAttr('disabled').attr('title', 'click to submit');
+      }
+    });
+  }
+submitState('#Login');
+submitState('#Register');
+submitState('#Password');
+
 $("#select_all").click(function(){
 
     if ($("input[type='checkbox']").prop("checked")) {
@@ -27,14 +56,39 @@ $(function() {
 	});
 });
 
-$('.control-checkbox input').change(function(){
-    if($(this).is(":checked")) {
-        $('.update-btn').addClass('d-flex');
-        $('.update-btn').removeClass('d-none');
-    } else {
-        $('.update-btn').addClass('d-none');
-        $('.update-btn').removeClass('d-flex');
-    }
+
+$('.control-checkbox input').change(function () {
+  if ($(this).is(":checked")) {
+    $('.update-btn').addClass("d-flex");
+  } else {
+      var flag=0;
+      $('.control-checkbox input').each(function(){
+          if ($(this).is(":checked")) {
+              $('.update-btn').addClass("d-flex");
+            flag=1;             
+          }
+          if(flag == 0){
+              $('.update-btn').removeClass('d-flex');
+          }
+      });
+  }
+});
+
+$('.form-check-input').change(function () {
+  if ($(this).is(":checked")) {
+    $('.update-btn').addClass("d-flex");
+  } else {
+      var flag=0;
+      $('.form-check-input').each(function(){
+          if ($(this).is(":checked")) {
+              $('.update-btn').addClass("d-flex");
+            flag=1;             
+          }
+          if(flag == 0){
+              $('.update-btn').removeClass('d-flex');
+          }
+      });
+  }
 });
 
 $(document).ready(function () {
